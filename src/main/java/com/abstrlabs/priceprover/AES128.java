e68b42c0908db4976fa66d03d134855b83de7b2d
nonojh <jones.zju@outlook.com>
2022-02-28 23:29:56 +0900

    add AES128 and padding function

---
+++ src/main/java/com/abstrlabs/priceprover/AES128.java
@@ -0,0 +1,51 @@
+package com.abstrlabs.priceprover;
+
+import com.abstrlabs.priceprover.util.Utility;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Arrays;
+
+
+public class AES128 {
+    public static final int GCM_TAG_LENGTH = 16;
+
+    public long[] aes128GcmDecrypt(long[] sr_padded, long[] cswk, long[] nswk, long[] csiv, long[] nsiv) throws Exception {
+        byte[] server_records0 = Utility.toByteArray(sr_padded);
+        byte[] client_swk_share = Utility.toByteArray(cswk);
+        byte[] notary_swk_share = Utility.toByteArray(nswk);
+        byte[] client_siv_share = Utility.toByteArray(csiv);
+        byte[] notary_siv_share = Utility.toByteArray(nsiv);
+        byte[] iv = Utility.xor(client_siv_share, notary_siv_share);
+        byte[] key = Utility.xor(client_swk_share, notary_swk_share);
+        byte[] nonce = Utility.concat(iv, Arrays.copyOfRange(server_records0, 0, 8));
+        byte[] tmp = new byte[] {(byte) 0x17, (byte) 0x03, (byte) 0x03};
+        byte[] tmp2 = Utility.toByteArray(server_records0.length - 8 - 16);
+        byte[] aad = Utility.concat(Utility.concat(Arrays.copyOfRange(server_records0, 0, 8), tmp), tmp2);
+        byte[] data = Arrays.copyOfRange(server_records0, 8, server_records0.length);
+
+        // Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        // Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        // Create GCMParameterSpec
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, nonce);
+        // Initialize Cipher for DECRYPT_MODE
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec);
+
+        cipher.updateAAD(aad);
+        // Perform Decryption
+        byte[] plainTextArr = cipher.doFinal(data);
+        String plainText = new String(plainTextArr);
+        System.out.println(plainText);
+
+        int content_start = plainText.indexOf("{");
+        int content_end = plainText.lastIndexOf("}");
+
+        byte[] content = Arrays.copyOfRange(plainTextArr, content_start, content_end + 1);
+        return Utility.toLongArray(content);
+    }
+
+}
+
+
---
+++ src/main/java/com/abstrlabs/priceprover/util/Utility.java
@@ -0,0 +1,68 @@
+package com.abstrlabs.priceprover.util;
+
+import java.math.BigInteger;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class Utility {
+    public static byte[] concat(byte[] first, byte[] second) {
+        byte[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+
+    public static long[] concat(long[] first, long[] second) {
+        long[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+
+    public static byte[] xor(byte[] arr1, byte[] arr2) {
+        byte[] result = new byte[arr1.length];
+
+        int i = 0;
+        for (byte b : arr1) {
+            result[i] = (byte) (b ^ arr2[i++]);
+        }
+
+        return result;
+    }
+
+    public static long[] toLongArray(byte[] arr) {
+        long[] result = new long[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (long) arr[i] & 0xff;
+        }
+        return result;
+    }
+
+    public static byte[] toByteArray(int num) {
+        return BigInteger.valueOf(num).toByteArray();
+    }
+
+    public static byte[] toByteArray(long[] arr) {
+        byte[] result = new byte[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = (byte) arr[i];
+        }
+        return result;
+    }
+
+    public static long[] padding(long[] arr) {
+        int len = arr.length;
+        int n = len / 64;
+        int mod = len % 64;
+        int retLength = (mod == 63) ? ((n + 2) * 64) : ((n + 1) * 64);
+        long[] ret = new long[retLength];
+        System.arraycopy(arr, 0, ret, 0, len);
+        ret[len] = 0x80;
+        for (int i = len + 1; i < retLength - 4; i++) {
+            ret[i] = 0;
+        }
+        for (int i = retLength - 4; i < retLength; i++) {
+            ret[i] = ((len * 8L) >> ((retLength - i - 1) * 8)) & 0xff;
+        }
+        return ret;
+    }
+
+}
